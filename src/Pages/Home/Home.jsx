9d1bd909ac225a018751983eff7265ae42d764f5
Sakif Ahmed <ahmed.sakif225@gmail.com>
2025-11-10 01:28:47 +0600

    Added private route

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import ThemeSwitcher from '../../Components/ThemeSwitcher/ThemeSwitcher';
 
 const Home = () => {
     return (
         <div>
             Home Page
+            <button className='btn btn-primary mr-5'>Primary</button>
+            <button className='btn btn-secondary'>Secondary</button>
+            <ThemeSwitcher></ThemeSwitcher>
         </div>
     );
 };
---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import useAuth from '../Hooks/useAuth';
+import { Navigate, useLocation } from 'react-router';
+
+const PrivateRoute = ({children}) => {
+
+    const {user, loading} = useAuth()
+    const location = useLocation()
+
+    if (loading) return <p>Loading</p>
+
+    if(user) return children
+
+    return <Navigate to="/auth/login/" state={{from:location}} replace />
+};
+
+export default PrivateRoute;
